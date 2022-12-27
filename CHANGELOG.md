1bd7294ef453882fad281b43ceaae50a166fdca9
Freek van de Ven <freek@freekvandeven.nl>
2022-12-27 10:19:05 +0100

    feat: add horizontal version of agenda

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+## [1.0.0] - 27 December 2022
+
+* Added option for custom Widget when there are no events for a given day
+* Support for both horizontal and vertical variant
+* Adjustable size for the component
+
 ## [0.0.1] - 2 September 2022
 
 * Initial release.
---
+++ lib/flutter_agenda.dart
@@ -0,0 +1,11 @@
+// SPDX-FileCopyrightText: 2022 Iconica
+//
+// SPDX-License-Identifier: BSD-3-Clause
+
+library agenda;
+
+export 'package:flutter_agenda/src/agenda.dart';
+export 'package:flutter_agenda/src/models/agenda_event.dart';
+export 'package:flutter_agenda/src/models/agenda_theme.dart';
+
+export 'package:timetable/timetable.dart';
